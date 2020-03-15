a6e83f958248a580b91100c16c6d147ea95eb817
René Bannat <r.bannat@gmail.com>
2020-03-15 20:31:39 +0100

    fix node id conflicts

--- gatsby-node.js
+++ gatsby-node.js
@@ -12,7 +12,6 @@ const {
 } = require('./lib/player')
 const { normalizeFixtures, processFixture } = require('./lib/fixture')
 
-const baseUrl = 'https://bettv.tischtennislive.de/Export/default.aspx'
 const associationsUrl = 'https://app.web4sport.de/ajax/Verband.ashx'
 
 exports.sourceNodes = async ({ actions, createNodeId }, configOptions) => {
@@ -27,137 +26,135 @@ exports.sourceNodes = async ({ actions, createNodeId }, configOptions) => {
   const associations = normalizeAssociations(
     await fetchAndParse(associationsUrl)
   )
-  await Promise.all(
-    leagueIds.map(async leagueId => {
-      const dataUrlFirstHalf = `https://app.web4sport.de/Ajax/Tischtennis/Staffel_Komplett.aspx?StaffelID=${leagueId}&PlanRunde=1&SpielerRunde=1`
-      const dataUrlSecondHalf = `https://app.web4sport.de/Ajax/Tischtennis/Staffel_Komplett.aspx?StaffelID=${leagueId}&PlanRunde=2&SpielerRunde=2`
-
-      const dataFirstHalf = await fetchAndParse(dataUrlFirstHalf)
-      const dataSecondHalf = await fetchAndParse(dataUrlSecondHalf)
-
-      // Create league
-      const league = normalizeLeague(dataFirstHalf.staffel)
-
-      // Create teams
-      let teams = normalizeTeams(dataFirstHalf.teams.mannschaft, league.id)
-      let players = []
-      const additionalTeamsData = teams
-        .map(team => {
-          const allPlayers = {}
-          const playersFirstHalf = []
-          const playersSecondHalf = []
-          const {
-            spieler: playersDataFirstHalf,
-          } = dataFirstHalf.teams.mannschaft.find(
-            mannschaft => mannschaft.teamid === team.id
-          ).bilanz
-          const {
-            spieler: playersDataSecondHalf,
-          } = dataSecondHalf.teams.mannschaft.find(
-            mannschaft => mannschaft.teamid === team.id
-          ).bilanz
-          if (playersDataFirstHalf && playersDataFirstHalf.length) {
-            playersDataFirstHalf.forEach(playerDataFirstHalf => {
-              const player = normalizePlayer(playerDataFirstHalf)
-              allPlayers[player.id] = {
-                name: player.name,
-                teamId: team.id,
-                playerScoresFirstHalf: getPlayerScores(player),
-              }
-              playersFirstHalf.push({
-                id: player.id,
-                position: player.position,
-              })
-            })
-          }
-          if (playersDataSecondHalf && playersDataSecondHalf.length) {
-            playersDataSecondHalf.forEach(playerDataSecondHalf => {
-              const player = normalizePlayer(playerDataSecondHalf)
-              allPlayers[player.id] = {
-                ...allPlayers[player.id],
-                teamId: team.id,
-                name: player.name,
-                playerScoresSecondHalf: getPlayerScores(player),
-              }
-              playersSecondHalf.push({
-                id: player.id,
-                position: player.position,
-              })
-            })
-          }
-
-          players.push(
-            ...Object.keys(allPlayers).map(key => ({
-              id: key,
-              ...allPlayers[key],
-            }))
-          )
-          return {
-            teamId: team.id,
-            playersFirstHalf,
-            playersSecondHalf,
-          }
-        })
-        .reduce((additionalTeamsData, additionalTeamData) => {
-          const teamId = additionalTeamData.teamId
-          delete additionalTeamData.teamId
-          additionalTeamsData[teamId] = additionalTeamData
-          return additionalTeamsData
-        }, {})
-
-      teams = teams.map(team => ({
-        ...team,
-        ...additionalTeamsData[team.id],
-      }))
-
-      // Create fixtures
-      const fixtures = [
-        ...normalizeFixtures(dataFirstHalf.spielplan.runde.spiel, true),
-        ...normalizeFixtures(dataSecondHalf.spielplan.runde.spiel, false),
-      ]
-
-      // Create association nodes
-      associations.forEach(association => {
-        createNode(
-          processAssociation({
-            association,
-            createNodeId,
-          })
-        )
+  // Create association nodes
+  associations.forEach(association => {
+    createNode(
+      processAssociation({
+        association,
+        createNodeId,
       })
+    )
+  })
 
-      // Create league node
+  for (const leagueId of leagueIds) {
+    const dataUrlFirstHalf = `https://app.web4sport.de/Ajax/Tischtennis/Staffel_Komplett.aspx?StaffelID=${leagueId}&PlanRunde=1&SpielerRunde=1`
+    const dataUrlSecondHalf = `https://app.web4sport.de/Ajax/Tischtennis/Staffel_Komplett.aspx?StaffelID=${leagueId}&PlanRunde=2&SpielerRunde=2`
+
+    const dataFirstHalf = await fetchAndParse(dataUrlFirstHalf)
+    const dataSecondHalf = await fetchAndParse(dataUrlSecondHalf)
+
+    // Create league
+    const league = normalizeLeague(dataFirstHalf.staffel)
+
+    // Create teams
+    let teams = normalizeTeams(dataFirstHalf.teams.mannschaft, league.id)
+    let players = []
+    const additionalTeamsData = teams
+      .map(team => {
+        const allPlayers = {}
+        const playersFirstHalf = []
+        const playersSecondHalf = []
+        const {
+          spieler: playersDataFirstHalf,
+        } = dataFirstHalf.teams.mannschaft.find(
+          mannschaft => mannschaft.teamid === team.id
+        ).bilanz
+        const {
+          spieler: playersDataSecondHalf,
+        } = dataSecondHalf.teams.mannschaft.find(
+          mannschaft => mannschaft.teamid === team.id
+        ).bilanz
+        if (playersDataFirstHalf && playersDataFirstHalf.length) {
+          playersDataFirstHalf.forEach(playerDataFirstHalf => {
+            const player = normalizePlayer(playerDataFirstHalf)
+            allPlayers[player.id] = {
+              name: player.name,
+              teamId: team.id,
+              playerScoresFirstHalf: getPlayerScores(player),
+            }
+            playersFirstHalf.push({
+              id: player.id,
+              position: player.position,
+            })
+          })
+        }
+        if (playersDataSecondHalf && playersDataSecondHalf.length) {
+          playersDataSecondHalf.forEach(playerDataSecondHalf => {
+            const player = normalizePlayer(playerDataSecondHalf)
+            allPlayers[player.id] = {
+              ...allPlayers[player.id],
+              teamId: team.id,
+              name: player.name,
+              playerScoresSecondHalf: getPlayerScores(player),
+            }
+            playersSecondHalf.push({
+              id: player.id,
+              position: player.position,
+            })
+          })
+        }
+
+        players.push(
+          ...Object.keys(allPlayers).map(key => ({
+            id: key,
+            ...allPlayers[key],
+          }))
+        )
+        return {
+          teamId: team.id,
+          playersFirstHalf,
+          playersSecondHalf,
+        }
+      })
+      .reduce((additionalTeamsData, additionalTeamData) => {
+        const teamId = additionalTeamData.teamId
+        delete additionalTeamData.teamId
+        additionalTeamsData[teamId] = additionalTeamData
+        return additionalTeamsData
+      }, {})
+
+    teams = teams.map(team => ({
+      ...team,
+      ...additionalTeamsData[team.id],
+    }))
+
+    // Create fixtures
+    const fixtures = [
+      ...normalizeFixtures(dataFirstHalf.spielplan.runde.spiel, true),
+      ...normalizeFixtures(dataSecondHalf.spielplan.runde.spiel, false),
+    ]
+
+    // Create league node
+    createNode(
+      processLeague({
+        league,
+        createNodeId,
+      })
+    )
+
+    // Create fixture nodes
+    fixtures.forEach(fixture => {
       createNode(
-        processLeague({
-          league,
+        processFixture({
+          fixture,
           createNodeId,
         })
       )
-
-      // Create fixture nodes
-      fixtures.forEach(fixture => {
-        createNode(
-          processFixture({
-            fixture,
-            createNodeId,
-          })
-        )
-      })
-      // Create player nodes
-      players.forEach(player => {
-        createNode(processPlayer({ player, createNodeId }))
-      })
-
-      // Create team nodes
-      teams.forEach(team => {
-        createNode(
-          processTeam({
-            team,
-            createNodeId,
-          })
-        )
-      })
     })
-  )
+    // Create player nodes
+    players.forEach(player => {
+      createNode(processPlayer({ player, createNodeId }))
+    })
+
+    // Create team nodes
+    teams.forEach(team => {
+      createNode(
+        processTeam({
+          team,
+          createNodeId,
+        })
+      )
+    })
+  }
   return
 }
